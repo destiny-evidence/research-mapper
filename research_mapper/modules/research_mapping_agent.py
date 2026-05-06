e4e35dd7e01b8765c58d9a86872cbba0286b11f6
VJonah <github@jonahv.dev>
2026-05-06 17:37:23 +0100

    fix: ResearchMappingAgent works without ui
    previously, ResearchMappingAgent didn't handle the absence of a TerminalUI instance passed to it on init
    
    this made live integration tests that don't test the UI fail unnecessarily
    
    now, ResearchMappingAgent has been refactored to handle the presence/absence of a ui

--- research_mapper/modules/research_mapping_agent.py
+++ research_mapper/modules/research_mapping_agent.py
@@ -1,6 +1,6 @@
 import dspy
 
-from research_mapper.models import UserQuery
+from research_mapper.models import UserQuery, Evidence
 from research_mapper.modules.screening_agent import ScreeningAgent
 from research_mapper.modules.search_agent import SearchAgent
 from research_mapper.ui import TerminalUI
@@ -13,15 +13,29 @@ class ResearchMappingAgent(dspy.Module):
         self.screening_agent = ScreeningAgent(tui=tui)
 
     def forward(self, user_query: UserQuery) -> dspy.Prediction:
-        self.tui.print_info("Generating suggested search queries:")
-        evidence = self.search_argent(user_query=user_query).evidence
-        self.tui.print_info(
-            f"{len(evidence)} pieces of evidence retrieved. Moving onto screening."
-        )
+        evidence = self._gather_evidence(user_query)
+        filtered_evidence = self._screen_evidence(user_query, evidence)
+        return dspy.Prediction(evidence=filtered_evidence)
+
+    def _gather_evidence(self, user_query: UserQuery) -> list[Evidence]:
+        if self.tui:
+            self.tui.print_info("Generating suggested search queries:")
+            evidence = self.search_argent(user_query=user_query).evidence
+            self.tui.print_info(
+                f"{len(evidence)} pieces of evidence retrieved. Moving onto screening."
+            )
+        else:
+            evidence = self.search_argent(user_query=user_query).evidence
+        return evidence
+
+    def _screen_evidence(
+        self, user_query: UserQuery, evidence: list[Evidence]
+    ) -> list[Evidence]:
         filtered_evidence = self.screening_agent(
             user_query=user_query, evidence=evidence
         ).screened_evidence
-        self.tui.print_info(
-            f"{len(evidence) - len(filtered_evidence)} piece(s) of evidence removed during screening. {len(filtered_evidence)} piece(s) of evidence remaining."
-        )
-        return dspy.Prediction(evidence=filtered_evidence)
+        if self.tui:
+            self.tui.print_info(
+                f"{len(evidence) - len(filtered_evidence)} piece(s) of evidence removed during screening. {len(filtered_evidence)} piece(s) of evidence remaining."
+            )
+        return filtered_evidence
